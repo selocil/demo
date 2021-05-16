0ebf315f9c4b0a5d463405f59c3bd641061ac9d1
selocil <selinciloglan@gmail.com>
2021-05-16 19:31:22 +0200

    Add mapping class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -51,5 +57,24 @@ public class DemoApplication {
 		return studentObjectMappedToJSONString;
 	}
 
+	@PostMapping("/student")
+	public String createPerson(@RequestParam(value = "name", defaultValue = "World") String name) {
+		Student student = new Student(name);
+		student.setAge(22);
+		student.setPhoneNumber("016738299789");
 
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+
+		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+		SessionFactory factory = meta.getSessionFactoryBuilder().build();
+		Session session = factory.openSession();
+
+		session.beginTransaction();
+
+		session.persist(student);
+		session.flush();
+		session.close();
+
+		return "Studierende/r wurde erfolgreich in der Datenbank persistiert.";
+	}
 }
